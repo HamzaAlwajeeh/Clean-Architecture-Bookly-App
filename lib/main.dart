2cbca80f0902df43af978c96e5fea8bfa8e56471
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-06 14:37:23 +0300

    add hive initialization and books box setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  Hive.initFlutter();
+
+  Hive.registerAdapter(BookEntityAdapter());
+
+  Hive.openBox<BookEntity>(kBooksBox);
   runApp(const BooklyApp());
 }
 
