a1d74bb7c5589d8780294c8f159d0731fa12071f
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-08 07:46:36 +0300

    add local storage for newset books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/api_service.dart';
 import 'package:bookly_app/core/functions/save_data_to_local_sourse.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -31,6 +32,8 @@ class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
           'volumes?Filtering=free-ebooks&q=subject:programming&sorting=newset',
     );
     List<BookEntity> books = getBooksList(data);
+    var box = Hive.box<BookEntity>(kNewsetBox);
+    box.addAll(books);
     return books;
   }
 
