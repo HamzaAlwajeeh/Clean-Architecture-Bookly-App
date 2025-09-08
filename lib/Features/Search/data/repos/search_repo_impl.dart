c5079abe84c88d76eacf74d61b1b3fcb951687ac
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-08 08:32:36 +0300

    implement search repository with error handling and finish logic of features

--- lib/Features/Search/data/repos/search_repo_impl.dart
+++ lib/Features/Search/data/repos/search_repo_impl.dart
@@ -1,14 +1,28 @@
+import 'package:bookly_app/Features/Search/data/data_source/search_remote_data_source.dart';
 import 'package:bookly_app/Features/Search/domain/repos/search_repo.dart';
 import 'package:bookly_app/core/entities/book_entity.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class SearchRepoImpl implements SearchRepo {
+  final SearchRemoteDataSource searchRemoteDataSource;
+
+  SearchRepoImpl({required this.searchRemoteDataSource});
   @override
   Future<Either<Failure, List<BookEntity>>> searchBooks({
     required String searchWord,
-  }) {
-    // TODO: implement searchBooks
-    throw UnimplementedError();
+  }) async {
+    try {
+      List<BookEntity> books = await searchRemoteDataSource.fetchSearchBooks(
+        searchWord: searchWord,
+      );
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
   }
 }
