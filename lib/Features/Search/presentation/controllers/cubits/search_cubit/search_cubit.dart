257eeac44b53e12d74c0396e117034012ea22042
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-09 13:54:41 +0300

    implement search cubit functionality and state management

--- lib/Features/Search/presentation/controllers/cubits/search_cubit/search_cubit.dart
+++ lib/Features/Search/presentation/controllers/cubits/search_cubit/search_cubit.dart
@@ -1,6 +1,18 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Search/domain/use_cases/search_books_use_case.dart';
 import 'package:bookly_app/Features/Search/presentation/controllers/cubits/search_cubit/search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  SearchCubit(this.searchBooksUseCase) : super(SearchInitial());
+  final SearchBooksUseCase searchBooksUseCase;
+
+  Future<void> searchBooks(String query) async {
+    emit(SearchLoading());
+    var result = await searchBooksUseCase.call(query);
+
+    result.fold(
+      (failure) => emit(SearchFailure(failure.errorMessage)),
+      (books) => emit(SearchSuccess(books)),
+    );
+  }
 }
