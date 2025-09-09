440a221c2570281156e184d98df9fccc17c01b5f
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-09 13:47:58 +0300

    implement newset books cubit functionality and state management

--- lib/Features/home/presentation/controllers/cubits/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/controllers/cubits/newset_books_cubit/newset_books_cubit.dart
@@ -1,6 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_newset_books_use_case.dart';
 import 'package:bookly_app/Features/home/presentation/controllers/cubits/newset_books_cubit/newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.fetchNewsetBooksUseCase) : super(NewsetBooksInitial());
+
+  final FetchNewsetBooksUseCase fetchNewsetBooksUseCase;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await fetchNewsetBooksUseCase.call();
+
+    result.fold(
+      (failure) => emit(NewsetBooksFailure(failure.errorMessage)),
+      (books) => emit(NewsetBooksSuccess(books)),
+    );
+  }
 }
