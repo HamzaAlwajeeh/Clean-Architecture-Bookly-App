b6dc234221e514b0bb1961f935c5f9873051e230
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-09 14:22:13 +0300

    feat: implement bloc providers for featured and newset books

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/feach_feature_books_use_case.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_newset_books_use_case.dart';
+import 'package:bookly_app/Features/home/presentation/controllers/cubits/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/controllers/cubits/newset_books_cubit/newset_books_cubit.dart';
 import 'package:bookly_app/core/constants/constants.dart';
 import 'package:bookly_app/core/entities/book_entity.dart';
+import 'package:bookly_app/core/functions/set_up_service_locator.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Hive.initFlutter();
-
+  setUpServiceLocator();
   Hive.registerAdapter(BookEntityAdapter());
 
   await Hive.openBox<BookEntity>(kFeaturedBox);
@@ -20,13 +27,31 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      title: 'Bookly App',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (context) => FeaturedBooksCubit(
+                FeachFeatureBooksUseCase(homeRepo: getIt.get<HomeRepoImpl>()),
+              )..fetchFeaturedBooks(),
+        ),
+        BlocProvider(
+          create:
+              (context) => NewsetBooksCubit(
+                FetchNewsetBooksUseCase(homeRepo: getIt.get<HomeRepoImpl>()),
+              )..fetchNewsetBooks(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        title: 'Bookly App',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
       ),
     );
   }
