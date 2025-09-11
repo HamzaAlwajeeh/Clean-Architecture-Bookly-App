cdddff77e4d400be2eec29bd2b39273f2c4f5810
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-11 13:49:43 +0300

    feat: implement similar books cubit functionality

--- lib/Features/home/presentation/controllers/cubits/similar_books_cubit/similar_books_cubit.dart
+++ lib/Features/home/presentation/controllers/cubits/similar_books_cubit/similar_books_cubit.dart
@@ -1,6 +1,19 @@
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_similar_books_use_case.dart';
 import 'package:bookly_app/Features/home/presentation/controllers/cubits/similar_books_cubit/similar_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
-  SimilarBooksCubit() : super(SimilarBooksInitial());
+  SimilarBooksCubit(this.fetchSimilarBooksUseCase)
+    : super(SimilarBooksInitial());
+  final FetchSimilarBooksUseCase fetchSimilarBooksUseCase;
+
+  Future<void> fetchSimilarBooks(String category) async {
+    emit(SimilarBooksLoading());
+    var result = await fetchSimilarBooksUseCase.call(category);
+    result.fold(
+      (failure) =>
+          emit(SimilarBooksFailure(errorMessage: failure.errorMessage)),
+      (books) => emit(SimilarBooksSuccess(books: books)),
+    );
+  }
 }
