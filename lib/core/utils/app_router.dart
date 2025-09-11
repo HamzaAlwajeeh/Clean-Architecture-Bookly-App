f8c824568d306a47eb63e34a4cb6d7f1421731d1
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-11 13:53:04 +0300

    feat: integrate SimilarBooksCubit into BookDetailsView route

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,7 +2,12 @@ import 'package:bookly_app/Features/Home/presentation/views/book_details_view.da
 import 'package:bookly_app/Features/Home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/Search/presentation/views/search_view.dart';
 import 'package:bookly_app/Features/Splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_similar_books_use_case.dart';
+import 'package:bookly_app/Features/home/presentation/controllers/cubits/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/core/entities/book_entity.dart';
+import 'package:bookly_app/core/functions/set_up_service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -16,8 +21,15 @@ abstract class AppRouter {
       GoRoute(
         path: kDetailsView,
         builder:
-            (context, state) =>
-                BookDetailsView(book: state.extra as BookEntity),
+            (context, state) => BlocProvider(
+              create:
+                  (context) => SimilarBooksCubit(
+                    FetchSimilarBooksUseCase(
+                      homeRepos: getIt.get<HomeRepoImpl>(),
+                    ),
+                  ),
+              child: BookDetailsView(book: state.extra as BookEntity),
+            ),
       ),
       GoRoute(
         path: kSearchView,
